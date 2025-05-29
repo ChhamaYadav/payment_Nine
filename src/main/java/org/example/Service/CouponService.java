842e459dbd99a87ddfdc03de47f89af745d708d7
ChhamaYadav <cy9nov97@gmail.com>
2025-05-29 16:24:25 +0530

    coupon applied tested{JSON}-sucess.

--- src/main/java/org/example/Service/CouponService.java
+++ src/main/java/org/example/Service/CouponService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class CouponService {
     public boolean applyCoupon(CouponRequest couponRequest) {
-        return "FIRST10".equals(couponRequest.getCouponCode());
+        return "FIRST10".equals(couponRequest.getCouponCode())||
+        "WELCOME100".equals(couponRequest.getCouponCode());
+
     }
 }
